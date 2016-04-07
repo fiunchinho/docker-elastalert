3cc3d39c2047f2fcc5f3ead2d29b825212e13569
José Armesto <github@armesto.net>
2016-04-07 10:03:20 +0200

    Passing different arguments to create index depending on existing env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,28 +2,24 @@
 
 rules_directory=${RULES_FOLDER:-/opt/elastalert/rules}
 es_port=${ELASTICSEARCH_PORT:-9200}
-aws_region=${AWS_REGION:-eu-west-1}
+aws_region=${AWS_REGION:-''}
 use_ssl=${USE_SSL:-False}
 
-# Render config file
-cat config.yaml | sed "s|es_host: [[:print:]]*|es_host: ${ELASTICSEARCH_HOST}|g" | sed "s|es_port: [0-9]*|es_port: $es_port|g" | sed "s|aws_region: [[:print:]]*|aws_region: $aws_region|g" | sed "s|use_ssl: [[:print:]]*|use_ssl: $use_ssl|g" | sed "s|rules_folder: [[:print:]]*|rules_folder: $rules_directory|g" > config.yaml.tmp
-cat config.yaml.tmp > config.yaml
-rm config.yaml.tmp
-
 # Render rules files
-for file in $(find $rules_directory -name '*.yaml' -or -name '*.yml');
+for file in $(find . -name '*.yaml' -or -name '*.yml');
 do
-    cat $file | sed "s|SNS_TOPIC_ARN|${SNS_TOPIC_ARN}|g" | sed "s|AWS_REGION|$aws_region|g" | sed "s|BOTO_PROFILE|${BOTO_PROFILE}|g" > rule
-    cat rule > $file
-    rm rule
+    cat $file | sed "s|es_host: [[:print:]]*|es_host: ${ELASTICSEARCH_HOST}|g" | sed "s|es_port: [[:print:]]*|es_port: $es_port|g" | sed "s|aws_region: [[:print:]]*|aws_region: $aws_region|g" | sed "s|boto_profile: [[:print:]]*|boto_profile: $BOTO_PROFILE|g" | sed "s|use_ssl: [[:print:]]*|use_ssl: $use_ssl|g" | sed "s|rules_folder: [[:print:]]*|rules_folder: $rules_directory|g" | sed "s|SNS_TOPIC_ARN|${SNS_TOPIC_ARN}|g" > config
+    cat config > $file
+    rm config
 done
 
-if ! wget ${ELASTICSEARCH_HOST}:${es_port}/elastalert_status 2>/dev/null
-then
-	echo "Creating Elastalert index in Elasticsearch..."
-    elastalert-create-index --index elastalert_status --old-index "" --no-auth
+echo "Creating Elastalert index in Elasticsearch..."
+if [ -n "$aws_region" ] && [ -n "$BOTO_PROFILE" ] ; then
+    elastalert-create-index --index elastalert_status --old-index "" --no-auth --aws-region $aws_region --boto-profile $BOTO_PROFILE;
+elif [ -n "$aws_region" ] ; then
+    elastalert-create-index --index elastalert_status --old-index "" --no-auth --aws-region $aws_region;
 else
-    echo "Elastalert index already exists in Elasticsearch."
+    elastalert-create-index --index elastalert_status --old-index "" --no-auth;
 fi
 
 exec "$@"
